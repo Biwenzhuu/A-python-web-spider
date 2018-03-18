2846d63e73ae39827fd5cf99c4536614426112df
Zhu Biwen <31669737+Biwenzhuu@users.noreply.github.com>
2018-03-19 00:56:22 +0100

    Delete server.py

--- web_app_framework/server.py
+++
@@ -1,8 +0,0 @@
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from wsgiref.simple_server import make_server
-from client import application
-httpd =make_server('', 8000, application)
-print('Serving HTTP on port 80000...')
-httpd.serve_forever()
